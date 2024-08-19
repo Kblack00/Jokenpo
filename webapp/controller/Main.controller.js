812d5d9498de02f065d1275fc10a1f3480e0e3ca
Kaue kauctz <kaue.kauctz@gmail.com>
2024-08-19 02:30:48 +0000

    versão inicial do app

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("exercicios.fiori.jokenpo.controller.Main", {
+        onInit: function () {
+
+        }
+    });
+});
